e527528f6594c9f615ac58aada883c2f57bdeb26
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-05-24 21:18:53 +0530

    Update Analysis.R

--- Analysis.R
+++ Analysis.R
@@ -1,4 +1,17 @@
-input_data
-master_data
-nrow(input_data)
-nrow(master_data)
+# 1) Find most frequently used SALE TYPE
+
+sort(table(input_data$`SALE TYPE`), decreasing = TRUE)
+barplot(table(input_data$`SALE TYPE`), main = "Most frequently SALE TYPE", xlab = "Sale Type", ylab = "Sales Count")
+
+# 2) Find most selling products***
+
+# 2.1) Select top 10 most selling products
+head(input_data[order(input_data$QUANTITY, decreasing = TRUE),], 10) 
+
+# 2.2) Select most selling products by selling quantities
+
+# Get selling quantities 
+table(input_data$QUANTITY)
+# Get selling quantities = 15
+subset()
+

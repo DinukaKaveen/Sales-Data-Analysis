fd363b860a1082f4e6d245108276ae06a7bb7f93
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-05-26 21:10:33 +0530

    Update Sales Analysis.R

---
+++ Sales Analysis.R
@@ -0,0 +1,18 @@
+# Minimum Sales & Product
+min(input_data$QUANTITY)
+subset(input_data, input_data$QUANTITY == min(input_data$QUANTITY))
+
+# Maximum Sales & Product
+max(input_data$QUANTITY)
+subset(input_data, input_data$QUANTITY == max(input_data$QUANTITY))
+
+# Sales Box plot by Selling Qty
+quantile(input_data$QUANTITY)
+q1 = quantile(input_data$QUANTITY)[2]
+q2 = quantile(input_data$QUANTITY)[3]
+q3 = quantile(input_data$QUANTITY)[4]
+
+iqr = q3 - q1
+print(iqr)
+
+boxplot(input_data$QUANTITY, xlab = "Sales", ylab = "Sales Qty", main = "Sales Distribution by Qty")

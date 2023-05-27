51cb0661f3255dd09f1f9337f422c74b4d0ca6e3
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-05-27 11:31:43 +0530

    Update Selling Analysis.R

--- Selling Analysis.R
+++ Selling Analysis.R
@@ -24,3 +24,14 @@ boxplot(input_data$QUANTITY ~ input_data$CATEGORY, xlab = "Category", ylab = "Sa
 plot(x = master_data$`BUYING PRIZE`, y = master_data$`SELLING PRICE`, xlab = "Buying Price", ylab = "Selling price", main = "Buying & Selling price correlation")
 cor(master_data$`BUYING PRIZE`, master_data$`SELLING PRICE`)
 
+# 4) Find most selling categories
+#--------------------------------
+
+# 4.1) Add new CATEGORY column to get category of each products
+pro_category <- master_data %>% select("PRODUCT ID", "CATEGORY")
+input_data <- inner_join(input_data, pro_category, by = c("PRODUCT ID"))
+
+# Create bar chart for the category wise sales
+barplot(tapply(input_data$QUANTITY, input_data$CATEGORY, FUN = sum), xlab = "Category", ylab = "Sales(Qty)", main = "Category wise sales")
+tapply(input_data$QUANTITY, input_data$CATEGORY, FUN = sum)
+
